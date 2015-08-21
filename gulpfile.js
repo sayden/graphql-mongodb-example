3c169b30ae539e2e13d226c75ad15b46d906bb76
Mario Castro <mariocaster@gmail.com>
2015-08-21 22:50:29 +0200

    babelify added

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,9 @@ var gulp = require('gulp');
 var plugins = require('gulp-load-plugins')();
 var babel = require('gulp-babel');
 var nodemon = require('gulp-nodemon');
+var fs = require("fs");
+var browserify = require("browserify");
+var babelify = require("babelify");
 
 /* Gulp variables */
 var config = {
@@ -12,11 +15,19 @@ var config = {
     entryFile: 'index.js'
 };
 
-gulp.task('default', function(){
+gulp.task('default',['babelify'], function(){
     nodemon({
         script:config.entryFile,
         ext:'js html es6',
         env: { 'NODE_ENV': 'development' }
     });
+});
+
+gulp.task('babelify', function(){
+  browserify("./js/App.es6", { debug: true })
+    .transform(babelify)
+    .bundle()
+    .on("error", function (err) { console.log("Error : " + err.message); })
+    .pipe(fs.createWriteStream("bundle.js"));
 });
 
