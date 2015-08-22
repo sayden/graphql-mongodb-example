2aa0356947637528fcdada5bdfc98e658654d2fe
Mario Castro <mariocaster@gmail.com>
2015-08-22 20:46:48 +0200

    Refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -12,20 +12,22 @@ var babelify = require("babelify");
 /* Gulp variables */
 var config = {
     appFolder: '.',
-    entryFile: 'index.js'
+    entryFile: 'index.js',
+    watch: ['js/**/*.es6'],
+  appFile: './js/App.es6'
 };
 
 gulp.task('default',['babelify'], function(){
     nodemon({
         script:config.entryFile,
-        ext:'js html es6',
+        ext:'js html es6 jsx',
         env: { 'NODE_ENV': 'development' }
     });
 });
 
 gulp.task('babelify', function(){
   browserify("./js/App.es6", { debug: true })
-    .transform(babelify)
+    .transform(babelify.configure({stage:0}))
     .bundle()
     .on("error", function (err) { console.log("Error : " + err.message); })
     .pipe(fs.createWriteStream("bundle.js"));
--- index.js
+++ index.js
@@ -1,2 +1,2 @@
 require('babel/register');
-require('./app.es6');
+require('./server.es6');
